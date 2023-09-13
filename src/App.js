d48370de05987ed2c713918a7cb96f93a6c6ddbf
Hasan Deeb <h.deeb712@gmail.com>
2023-09-13 10:28:02 +0300

    created the html structure inside App.js and css inside the App.css with responsive view

--- src/App.js
+++ src/App.js
@@ -1,32 +1,87 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
+import  clear from './img/weather-icons/clear.svg';
+import  mostlyCloudy from './img/weather-icons/mostlycloudy.svg';
 
 import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Karim"
+const App = () => {
+  
+    const handleInputChange = value => {
+      this.setState({ name: value });
     };
-  }
+  
 
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
-
-  render() {
     return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+      <div classNameName="app">
+        <header>
+        <input type="text" className="search" placeholder="Type in a city name"/>
+        <button className="btn find-weather-btn">Find Weather</button>
+    </header>
+    
+    <main>
+        <section className="today-weather">
+            <img src={mostlyCloudy} alt="Weather"/>
+            <section className="caption-on-weather">overcast clouds</section>
+            <section id="today-weather-details">
+            <section className="temp">
+                    <span>Temperature</span>10 to 11 c
+                </section>
+                <section className="other">
+                    <section className="Humidity">
+                        <span>Humidity</span> 78%   
+                </section>
+                <section className="pressure">
+                    <span>Pressure</span> 1008.48
+                </section>
+            </section>
+        </section>
+    </section>
+        
+        <section id="basis-container">
+            <section className="condition-container">
+                <i className="time">3:00</i>
+                <figure><img className="weather-condition" src={mostlyCloudy} alt="clear-weather" /></figure>
+                <i className="temprature">8&deg;C</i>
+            </section>
+            <section className="condition-container">
+                <i className="time">6:00</i>
+                <figure><img className="weather-condition" src={mostlyCloudy} alt="clear-weather" /></figure>
+                <i className="temprature">9&deg;C</i>
+            </section>
+            <section className="condition-container">
+                <i className="time">9:00</i>
+                <figure><img className="weather-condition" src={clear} alt="clear-weather" /></figure>
+                <i className="temprature">14&deg;C</i>
+            </section>
+            <section className="condition-container">
+                <i className="time">12:00</i>
+                <figure><img className="weather-condition" src={clear} alt="clear-weather" /></figure>
+                <i className="temprature">17&deg;C</i>
+            </section>
+            <section className="condition-container">
+                <i className="time">15:00</i>
+            <figure><img className="weather-condition" src={clear} alt="clear-weather" /></figure>
+            <i className="temprature">18&deg;C</i>
+        </section>
+        <section className="condition-container">
+            <i className="time">18:00</i>
+            <figure><img className="weather-condition" src={clear} alt="clear-weather" /></figure>
+            <i className="temprature">16&deg;C</i>
+        </section>
+        <section className="condition-container">
+            <i className="time">21:00</i>
+            <figure><img className="weather-condition" src={mostlyCloudy} alt="clear-weather" /></figure>
+            <i className="temprature">13&deg;C</i>
+        </section>
+    </section>
+    </main>
       </div>
     );
   }
-}
+
 
 export default App;
